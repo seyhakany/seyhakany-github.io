c1ddf63d98fc74e384b7cd6596c5ddd14d319ab1
Anthony <anthony.chheng@pathmazing.com>
2023-08-19 02:00:21 +0700

    update()

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -121,7 +121,7 @@ const HomePage = () => {
             />
           </div>
           <a
-            href="https://github.com/seyhakany/seyhakany-github.io/resume.pdf"
+            href="https://seyhakany.github.io/seyhakany-github.io/src/assets/files/resume.pdf"
             target="_blank"
           >
             <CustomButton sx={{ padding: "5px 10px" }}>Resume</CustomButton>
