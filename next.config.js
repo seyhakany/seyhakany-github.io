8650f64b05db87dec62a15560e08caea82d1aa7e
your_github_username <anthony.chheng@pathmazing.com>
2024-04-23 01:19:10 +0700

    fix(): toggle theme

--- next.config.js
+++ next.config.js
@@ -5,15 +5,15 @@ const nextConfig = {
     styledComponents: true,
   },
   // !!! for production only
-  output: "export",
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.pdf$/i,
-      type: "asset/source",
-    });
+  // output: "export",
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.pdf$/i,
+  //     type: "asset/source",
+  //   });
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
